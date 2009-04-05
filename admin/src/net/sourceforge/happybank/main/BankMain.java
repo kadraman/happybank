c1e0c11af3f66b0f2cd6ab91a200d50264a038a6
kevinalee <kevinalee@me.com>
2009-04-05 17:55:30 +0000

    Added Apache ivy setup

--- admin/src/net/sourceforge/happybank/main/BankMain.java
+++ admin/src/net/sourceforge/happybank/main/BankMain.java
@@ -342,6 +342,6 @@ public class BankMain {
     /*
      * constants
      */
-    static final private String APP_NAME = "Happy Bank Admin Client";
+    static final private String APP_NAME = "HappyBank Admin Client";
 
 } // BankAdminMain
