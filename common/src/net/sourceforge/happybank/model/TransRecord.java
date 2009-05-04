ed95caf3d0580796dea44250e8ecf5510dcc7747
kevinalee <kevinalee@me.com>
2009-05-04 17:09:40 +0000

    Major refactoring of directories

---
+++ common/src/net/sourceforge/happybank/model/TransRecord.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2005-2009 Kevin A. Lee
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.sourceforge.happybank.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import org.joda.time.DateTime;
+
+/**
+ * Java bean representing an account transaction.
+ * 
+ * @author Kevin A. Lee
+ * @email kevin.lee@buildmeister.com
+ */
+
+public class TransRecord implements Comparable, Serializable {
+    
+    /**
+     * Generated serialization identifier.
+     */
+    private static final long    serialVersionUID = 1L;
+    /**
+     * Timestamp of the transaction.
+     */
+    private DateTime             timeStamp;
+    /**
+     * The account the transaction is being applied to.
+     */
+    private String               accid;
+    /**
+     * The type of the transaction, e.g. credit or debit.
+     */
+    private String               transType;
+    /**
+     * The amount of the transaction.
+     */
+    private java.math.BigDecimal transAmt;
+    
+    /**
+     * Default no-arg constructor.
+     */
+    public TransRecord() {
+        super();
+    } // TransRecord
+    
+    /**
+     * Default constructor to create new transaction.
+     * 
+     * @param type type of transaction (credit or debit)
+     * @param amount amount of transaction
+     */
+    public TransRecord(final String type, final BigDecimal amount) {
+        setTimeStamp(new DateTime());
+        setTransType(type);
+        setTransAmt(amount);
+    } // TransRecord
+    
+    /**
+     * Get time stamp of transaction.
+     * 
+     * @return time stamp of transaction
+     */
+    public final DateTime getTimeStamp() {
+        return timeStamp;
+    } // getTimeStamp
+    
+    /**
+     * Get the account the transaction has been applied to.
+     * 
+     * @return the account id
+     */
+    public String getAccid() {
+        return accid;
+    } // getAccid
+    
+    /**
+     * Get transaction type.
+     * 
+     * @return transaction type (credit or debit)
+     */
+    public final String getTransType() {
+        return transType;
+    } // getTransType
+    
+    /**
+     * Get the amount of the transaction.
+     * 
+     * @return amount of transaction
+     */
+    public final java.math.BigDecimal getTransAmt() {
+        return transAmt;
+    } // getTransAmt
+    
+    /**
+     * Set time stamp of transaction.
+     * 
+     * @param aTimeStamp time stamp of transaction
+     */
+    public final void setTimeStamp(final DateTime aTimeStamp) {
+        this.timeStamp = aTimeStamp;
+    } // setTimeStamp
+    
+    /**
+     * Set the account the transactions is being applied to.
+     * 
+     * @param aid the account
+     */
+    public void setAccid(String aid) {
+        this.accid = aid;
+    } // setAccid
+    
+    /**
+     * Set transaction type.
+     * 
+     * @param aTransType type of transaction (credit or debit)
+     */
+    public final void setTransType(final String aTransType) {
+        this.transType = aTransType;
+    } // setTransType
+    
+    /**
+     * Set the amount of the transaction.
+     * 
+     * @param aTransAmt amount of the transaction
+     */
+    public final void setTransAmt(final BigDecimal aTransAmt) {
+        this.transAmt = aTransAmt;
+    } // setTransAmt
+    
+    /**
+     * Compare two TransRecords: required to enable sorting of transactions.
+     * 
+     * @param anObject transaction to compare with
+     * @return 1 if transaction records are equal else -1
+     */
+    public final int compareTo(final Object anObject) {
+        TransRecord other = (TransRecord) anObject;
+        if (getTimeStamp().isEqual(other.getTimeStamp())) {
+            return 1;
+        } else {
+            return -1;
+        }
+    } // compareTo
+    
+    /**
+     * Formatted output of account.
+     * 
+     * @return formatted output
+     */
+    public final String toString() {
+        return "TransRect " + timeStamp + " " + transType + " " + transAmt;
+    } // toString
+    
+} // TransRecord
