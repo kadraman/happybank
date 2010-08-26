60b545fc8bbc41e2be063a926e4cf7b9c93b094e
kevinalee <kevinalee@me.com>
2010-08-26 10:38:12 +0000

    Updating to MySQL

--- maven/trunk/db/src/main/resources/runServer.bat
+++
@@ -1,2 +0,0 @@
-cd data
-java -classpath C:\JavaTools\hsqldb\lib\hsqldb.jar org.hsqldb.Server -database.0 file:happybank -dbname.0 xdb
--- maven/trunk/db/src/main/resources/sql/testdata/customers.sql
+++ maven/trunk/db/src/main/resources/sql/testdata/customers.sql
@@ -1,6 +1,6 @@
-INSERT INTO PUBLIC.CUSTOMER(customerid,title,firstname,lastname,userid,password) VALUES (101,'Col','William','Mustard','cust101','password');
-INSERT INTO PUBLIC.CUSTOMER(customerid,title,firstname,lastname,userid,password) VALUES (102,'Pro','Percival','Plum','cust102','password');
-INSERT INTO PUBLIC.CUSTOMER(customerid,title,firstname,lastname,userid,password) VALUES (103,'Rev','Mansfied','Green','cust103','password')
-INSERT INTO PUBLIC.CUSTOMER(customerid,title,firstname,lastname,userid,password) VALUES (104,'Mrs','Elizabeth','Peacock','cust104','password');
-INSERT INTO PUBLIC.CUSTOMER(customerid,title,firstname,lastname,userid,password) VALUES (105,'Mis','Samantha','Scarlett','cust105','password');
-INSERT INTO PUBLIC.CUSTOMER(customerid,title,firstname,lastname,userid,password) VALUES (106,'Mrs','Maud','White','cust106','password');
+INSERT INTO PUBLIC.CUSTOMERS(id,title,firstname,lastname,userid,password) VALUES (101,'Col','William','Mustard','cust101','password');
+INSERT INTO PUBLIC.CUSTOMERS(id,title,firstname,lastname,userid,password) VALUES (102,'Pro','Percival','Plum','cust102','password');
+INSERT INTO PUBLIC.CUSTOMERS(id,title,firstname,lastname,userid,password) VALUES (103,'Rev','Mansfied','Green','cust103','password')
+INSERT INTO PUBLIC.CUSTOMERS(id,title,firstname,lastname,userid,password) VALUES (104,'Mrs','Elizabeth','Peacock','cust104','password');
+INSERT INTO PUBLIC.CUSTOMERS(id,title,firstname,lastname,userid,password) VALUES (105,'Mis','Samantha','Scarlett','cust105','password');
+INSERT INTO PUBLIC.CUSTOMERS(id,title,firstname,lastname,userid,password) VALUES (106,'Mrs','Maud','White','cust106','password');
