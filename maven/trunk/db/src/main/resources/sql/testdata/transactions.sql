5f84f5976e0c827e462dbe97fd0502e263202feb
kevinalee <kevinalee@me.com>
2011-09-09 15:00:52 +0000

    Refactoring

--- maven/trunk/db/src/main/resources/sql/testdata/transactions.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO TRANSRECORD(accid,transdate,transtype,transamt) VALUES ('101-1001',NOW(),'C',100.50);
-INSERT INTO TRANSRECORD(accid,transdate,transtype,transamt) VALUES ('101-1001',NOW(),'C',90.45);
-INSERT INTO TRANSRECORD(accid,transdate,transtype,transamt) VALUES ('101-1001',NOW(),'D',10.00);
-INSERT INTO TRANSRECORD(accid,transdate,transtype,transamt) VALUES ('101-1001',NOW(),'C',2310.23);
-INSERT INTO TRANSRECORD(accid,transdate,transtype,transamt) VALUES ('101-1001',NOW(),'D',1202.55);
-INSERT INTO TRANSRECORD(accid,transdate,transtype,transamt) VALUES ('101-1001',NOW(),'C',500.23);
